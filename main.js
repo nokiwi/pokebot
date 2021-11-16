d80fa0386f898b29565af2d1e005539af2d827dd
nokiwi <81134724+nokiwi@users.noreply.github.com>
2021-11-15 23:14:17 -0800

    Pokemon text file change

--- main.js
+++ main.js
@@ -3,24 +3,35 @@ const auth = require('./auth.json');
 const {prefix, token} = require('./auth.json');
 const client = new Discord.Client();
 
-/*
+// import fs module
+var fs = require("fs");
+fs.readFile("./pokemon.txt", function(text) {
+    var pokemon = fs.readFileSync("./pokemon.txt").toString('utf-8');
+});
+
 client.on('ready', () => {
     console.log("Connected as " + client.user.tag);
-    client.user.setActivity("Listening for input...");
+    //client.user.setActivity("Ready!");
 });
-*/
 
 client.on('message', (receivedMessage) => {
     if (receivedMessage.author == client.user) { return }
     
+    catchPokemon(receivedMessage);
+
     processMessage(receivedMessage);
 });
+
+async function catchPokemon(receivedMessage) {
+    console.log()
+    receivedMessage.reply("you caught a " + pokemon[Math.floor.random() * pokemon.length]);
+}
 
 async function processMessage(receivedMessage) {
     console.log(receivedMessage.content + " received");
 
-    if(receivedMessage.content == "snap crackle pop") {
-        receivedMessage.reply("POPOPOPOP");
+    if(receivedMessage.content == "hello") {
+        receivedMessage.reply("hi" + receivedMessage.author);
     }
     else if(receivedMessage.content == "jetay") {
         receivedMessage.reply("hi my name's jetay and i'm a rock paper scissors GOD");
